b9c5214da2d8133e9fcf7081528ff28dfb9ac266
VitaliSantalau <v.santalau@gmail.com>
2021-08-01 18:10:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const OPEN_BRACETS = bracketsConfig.map(arr => arr[0]);
+  const BRACETS_PAIR = bracketsConfig.reduce((obj, arr) => {
+    obj[arr[1]] = arr[0];
+    return obj;
+  }, {});
+
+  for(let i = 0; i < str.length; i++) {
+    const current = str[i];
+    if(!stack.length) {
+      if(OPEN_BRACETS.includes(current)) {
+        stack.push(current);
+      } else return false;
+    } else if(BRACETS_PAIR[current] === stack[stack.length-1]) {
+        stack.pop();
+      } else if(OPEN_BRACETS.includes(current)) {
+        stack.push(current);
+        } else return false;
+    }
+    
+    return stack.length === 0;
+  }
